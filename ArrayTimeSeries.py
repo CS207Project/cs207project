967a1246e49d11512d6c13054cfc9268902d9c2c
Juaquin <jnikksanchez@gmail.com>
2016-02-10 16:16:37 -0500

    clean ArrayTimeSeries, remove unnecessary implementations

--- ArrayTimeSeries.py
+++ ArrayTimeSeries.py
@@ -1,9 +1,9 @@
 import itertools
 import numpy as np
-#from TimeSeries import TimeSeries
+from TimeSeries import TimeSeries
 
 
-class ArrayTimeSeries():
+class ArrayTimeSeries(TimeSeries):
     """
     A class for representing a time series,
     initiated with data stored as a numpy array.
@@ -23,32 +23,16 @@ class ArrayTimeSeries():
     def __init__(self, data):
         self._data = np.array(data)
 
-    def __len__(self):
-        return len(self._data)
-
-    def __getitem__(self, position):
-        return self._data[position]
-
-    def __setitem__(self, position, value):
-        self._data[position] = value
-
-    def __repr__(self):
-        class_name = type(self).__name__
-        return "{}({})".format(class_name, self._data[1:5])
-
-    def __str__(self):
-        class_name = type(self).__name__
-        return "{}".format(self._data[1:5])
-
-threes = ArrayTimeSeries(range(0, 1000, 3))
-fives = ArrayTimeSeries(range(0, 1000, 5))
-print(repr(threes))
-print(fives)
+# smoke test
+# threes = ArrayTimeSeries(range(0, 1000, 3))
+# fives = ArrayTimeSeries(range(0, 1000, 5))
+# print(repr(threes))
+# print(fives)
 
 
-s = 0
-for i in range(0, 1000):
-    if i in threes or i in fives:
-        s += i
+# s = 0
+# for i in range(0, 1000):
+#     if i in threes or i in fives:
+#         s += i
 
-print("sum", s)
+# print("sum", s)
