48e64b94de8984d44fa1d87ba66f6e4cc6e567e8
christianjunge <christianjunge@g.harvard.edu>
2016-02-19 13:28:53 -0500

    Updated documentation

--- TimeSeries2.py
+++ TimeSeries2.py
@@ -2,32 +2,64 @@ import reprlib
 import itertools
 import numpy as np
 
+from doctest import run_docstring_examples as dtest
+
 
 class TimeSeries:
     """
-    a = TimeSeries([1, 1.5, 2, 2.5, 10], [0, 2, -1, 0.5, 0])
-    a[2.5] = 1.0
-
-    print a[0]
-        error
-
-    a.contains(1)
-        returns True
-
-    a.contains(20)
-        returns False
-
-    len(a)
-        returns 5
-
-    [v for v in TimeSeries([0,1,2],[1,3,5])] 
-        returns [1,3,5]
-
-    str(a)
-        returns 
+    Attributes
+    ----------
+    times : list of times that data were collected
+    values: magnitudes of observations at each time
 
 
-    experiment
+    Methods
+    -------
+    __init__: instantiate TimeSeries class object
+    __len__: length of TimeSeries
+    get_index: Helper function to get the array index of a time
+    __getitem__: Returns value at a given time
+    __setitem__: Set value at a given time
+    __contains__: Truth value of a given time
+    __repr__: Represents TimeSeries
+    __str__: Returns TimeSeries as a string
+
+    Tests
+    -----
+    >>> a = TimeSeries([1, 1.5, 2, 2.5, 10], [0, 2, -1, 0.5, 0])
+    >>> a[2.5] = 1.0
+
+    >>> print a[10]
+    0
+
+    >>> print a[0]
+        Traceback 
+        ...
+        IndexError: Time not in Time Series
+
+    >>> a.contains(1)
+    True
+
+    >>> a.contains(20)
+    False
+
+    >>> len(a)
+    5
+
+    >>> [v for v in TimeSeries([0,1,2],[1,3,5])] 
+    [1,3,5]
+
+    >>> str(a)
+    returns [1.0, 1.5, 2.0, 2.5, 10.0], [0, 2, -1, 1.0, 0]
+
+    >>> a.values()
+    [0, 2, -1, 1.0, 0]
+
+    >>> a.times()
+    [1.0, 1.5, 2.0, 2.5, 10.0]
+
+    >>> a.items()
+    [(1.0, 0), (1.5, 2), (2.0, -1), (2.5, 1.0), (10.0, 0)]
 
     """
 
@@ -89,4 +121,6 @@ print(a[2.5])
 
 # This should return an error, because there is no time point at t=0
 # a[0]
-print(str(a))
+
+##### To run doctest:
+# dtest(TimeSeries, globals(), verbose = True)
