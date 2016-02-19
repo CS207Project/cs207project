efe0251dc275f7f6ba387e9739761a314b735f77
Akhil Ketkar <akhilketkar@gmail.com>
2016-02-19 14:17:09 -0500

    finished writing a lazy evaluation method

--- lazy.py
+++ lazy.py
@@ -1,36 +1,51 @@
-
 class LazyOperation:
-
-    def __init__(self,old_fuction,*args,**kwargs):
+    def __init__(self,old_fuction,args_a,kwargs_a):
         self._function = old_fuction
-        self._args = args
-        self._kwargs = kwargs
-    '''
-    def __eq__(self,other):
-        print("in eq")
-        print(type(self).__name__)
-        print(type(other).__name__)
-        return id(self) == id(other)
-    '''
-    def eval(self):
-        return self._function(*self._args,**self._kwargs)
+        self._args = args_a
+        self._kwargs = kwargs_a
 
+    def __eq__(self,other):
+        retval = (self._args == other._args) and (self._kwargs == other._kwargs)
+        retval = retval and (id(self._function) == id(other._function))
+        return retval
+
+    def eval(self):
+        computedArgs = []
+        computedKWArgs = []
+
+        # positional args
+        for a in self._args:
+            if a.__class__.__name__ == "LazyOperation":
+                computedArgs.append(a.eval())
+            else:
+                computedArgs.append(a)
+
+        # keyword args
+        for k,v in self._kwargs.items():
+            if v.__class__.__name__ == "LazyOperation":
+                computedArgs.append((k,v.eval()))
+            else:
+                computedArgs.append((k,v))
+        return self._function(*tuple(computedArgs),**dict(computedKWArgs))
+
+# this is the decorator
 def lazy(old_fuction):
     def decorated_func(*args,**kwargs):
-        return LazyOperation(old_fuction,*args,**kwargs)
-
+        return LazyOperation(old_fuction,args_a = args,kwargs_a = kwargs)
     return decorated_func
 
-@lazy
-def lazy_add(a,b):
+# raw functions
+def add(a,b):
     return a+b
+def mul(a,b):
+    return a*b
 
-l = lazy_add(1,2)
+# decorated functions
+lazy_add = lazy(add)
+lazy_mul = lazy(mul)
 
-print(id(l))
-print(id(lazy_add(1,2)))
-print(id(LazyOperation(lazy_add,args=[1,2],kwargs={})))
-
-assert(lazy_add(1,2) == LazyOperation(lazy_add,args=[1,2],kwargs={}))
-#ladd = lazy_add(1,2)
-#print(ladd.eval())
+# print(id(lazy_add(1,2)))
+# print(id(LazyOperation(add,args=[1,2],kwargs={})))
+# print(lazy_add(1,2) == LazyOperation(add,args_a = (1,2),kwargs_a={}))
+print(isinstance( lazy_add(1,2), LazyOperation ) == True)
+print(lazy_add(lazy_mul(3,4),5).eval())
