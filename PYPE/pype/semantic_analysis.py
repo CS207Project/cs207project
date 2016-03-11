396bdb317a05e480415bf31d6154344b03a2ca3f
Daniel Yue <yuedaniel@gmail.com>
2016-03-11 12:21:39 -0500

    added skeleton files for Friday

--- PYPE/pype/semantic_analysis.py
+++ PYPE/pype/semantic_analysis.py
@@ -1,5 +1,13 @@
 from .ast import *
 
+class PrettyPrint(ASTVisitor):
+  def __init__(self):
+    pass
+  def visit(self, node):
+    pass # TODO
+
 class CheckSingleAssignment(ASTVisitor):
-  def __init__(self): pass # TODO
-  def visit(self, node): pass # TODO
+  def __init__(self):
+    pass # TODO
+  def visit(self, node):
+    pass # TODO
